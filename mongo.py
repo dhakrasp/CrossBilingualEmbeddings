88141140177c1f03c9de1667330b528a4bf1901b
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-11-04 21:27:34 +0530

    Adding mongo script to dump vocab

--- mongo.py
+++ mongo.py
@@ -1,23 +1,25 @@
 from pymongo import MongoClient
 import glob
 import os
+import re
 
 if __name__ == '__main__':
     client = MongoClient('localhost', 27017)
     db = client['nlprokz']
-    glove = db.glove
+    glove = db.hinglish
     count = 0
-    for root, dirs, files in os.walk('Glove/'):
+    for root, dirs, files in os.walk('Corpus/Hindi_English/'):
         for basename in files:
             filename = os.path.join(root, basename)
             bulk_grams = []
             for line in open(filename):
-                grams = line.split()
-                word = grams[0]
-                rest = grams[1:]
-                bulk_grams.append({"gram":word,"glove_vector":rest})
-                if len(bulk_grams) > 5000:
-                    count += 5000
-                    print 'Inserted '+str(count)
-                    glove.insert_many(bulk_grams)
-                    bulk_grams = []
+                grams = re.split(r'\s',line.strip().replace("\\"," "))
+                id = grams[0]
+                sentence = grams[1:]
+                words = sentence[::2]
+                pos_tags = sentence[1::2]
+                bulk_grams.append({"identifier":id,"words":words,"pos_tags":pos_tags})
+
+            count += len(bulk_grams)
+            print 'Inserted '+str(count)
+            glove.insert_many(bulk_grams)
