ea7e37008d8fbc2fbb44ec2f566b15e561866c2c
Sahil Chelaramani <sahilc.2200@gmail.com>
2016-11-10 16:49:32 +0530

    Mongo bugfix [whoops]

--- mongo_new.py
+++ mongo_new.py
@@ -53,5 +53,45 @@ def dump_glove_matrix(lang_origin,lang_target):
         except Exception as e:
             print e
 
+def find_closest(lang_origin,lang_target):
+    client = MongoClient("localhost", 27017)
+    db = client["nlprokz"]
+    target_words = []
+    target_words_strings = []
+    for i in db.bilingual_glove_vec.find({"lang_origin":lang_target}):
+        target_words.append(i['vec'])
+        target_words_strings.append(i['word'])
+
+    words = sorted(db.hinglish.find({"lang":lang_target}).distinct("words"))
+
+    target_words = np.transpose(np.array(target_words))
+    target_words =  target_words/ np.linalg.norm(target_words,axis = 0)
+
+    count = 0
+    for i in db.bilingual_glove_vec.find({"lang_origin":lang_origin}):
+        origin_word = 1.0*np.array([i['vec']])
+        origin_word = origin_word/np.linalg.norm([i['vec']])
+
+        # dist = []
+        # for word_vec in target_words:
+        #     word_vec = np.transpose(word_vec)
+        #     dist.append(euclidean(origin_word, word_vec))
+        # max_similarity = np.argmax(dist[0])
+        # print "STRING:"+i['word']+":"+target_words_strings[max_similarity]
+        # count += 1
+        # if count > 10:
+        #    break
+
+        dot_product = np.dot(origin_word,target_words)
+        max_index = dot_product.argsort()[-1:][::-1]
+        for j in max_index[0][-10:]:
+           print "STRING %s: %s : %s" % (i['word'],target_words_strings[j],str(dot_product[0][j]))
+        # max_similarity = np.argmax(dot_product[0])
+        # print "STRING:"+i['word']+":"+target_words_strings[max_similarity]+":"+str(dot_product[0][max_similarity])
+        count += 1
+        if count > 10:
+           break
+
 if __name__ == '__main__':
-    dump_glove_matrix("eng","eng")
+    #dump_glove_matrix("eng","eng")
+    find_closest("hin","eng")
